67d1fc32cc9fd6450ae486ce716e3d27517bfae2
maestrofly <derrickalberto@ymail.com>
2024-07-16 09:34:41 +0000

    server created using express

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('Hello, World!');
+const express = require('express');
+const app = express();
+
+app.listen (3000, ()=> {
+    console.log('Server is running http://localhost:3000');
+})
